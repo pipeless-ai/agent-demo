20beb9a22dc560c689574495ffdb42cb083077d2
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-04-10 18:32:06 +0200

    Changes
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- agent.py
+++ agent.py
@@ -1,6 +1,18 @@
 from pipeless_agents_sdk.cloud import data_stream
+from pymongo.mongo_client import MongoClient
 
-print(f"Listening agent messages")
+uri = "mongodb+srv://miguel:md3IwUQKZg10fqNf@yc-demo.f43lerr.mongodb.net/?retryWrites=true&w=majority&appName=yc-demo"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client["yc_demo"]
+collection = db["yc_demo"]
 for payload in data_stream:
-    print(f"New data received: {payload.value}")
+  print(f"New data received: {payload.value}, sending to MongoDB")
+  try:
+    collection.insert_one(payload.value)
+    print("Data stored in MongoDB!")
+  except Exception as e:
+    print(e)
+
+
 
