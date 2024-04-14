2cf2a20832cb1a5e0e25816d3127ce0a17fdcba6
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-04-14 13:16:05 +0200

    Update demo
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- agent.py
+++ agent.py
@@ -1,11 +1,11 @@
 from pipeless_agents_sdk.cloud import data_stream
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://miguel:md3IwUQKZg10fqNf@yc-demo.f43lerr.mongodb.net/?retryWrites=true&w=majority&appName=yc-demo"
-# Create a new client and connect to the server
+uri = "mongodb+srv://miguel:md3IwUQKZg10fqNf@demo.ga9t9gq.mongodb.net/?retryWrites=true&w=majority&appName=yc-demo"
 client = MongoClient(uri)
 db = client["yc_demo"]
 collection = db["yc_demo"]
+
 for payload in data_stream:
   print(f"New data received: {payload.value}, sending to MongoDB")
   try:
